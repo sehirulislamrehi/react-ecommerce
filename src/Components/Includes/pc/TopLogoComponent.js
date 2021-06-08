87e9e5ab2a5edf368e70db24b3933adbaeaa916e
Sehirul Islam Rehi <mdsehirulislamrehi@gmail.com>
2021-06-08 07:08:22 +0600

    Complete

--- src/Components/Includes/pc/TopLogoComponent.js
+++ src/Components/Includes/pc/TopLogoComponent.js
@@ -2,9 +2,12 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import GetCategoryData from "../../../Functionality/GetCategoryData";
 
+import { useLocation } from 'react-router-dom'
+
 import Swal from "sweetalert2"
 import withReactContent from 'sweetalert2-react-content'
 const MySwal = withReactContent(Swal)
+
 
 const TopLogoComponent = () => {
 
@@ -13,7 +16,8 @@ const TopLogoComponent = () => {
     const [cart_length, set_cart_length ] = useState()
 
 
-    
+    const location = useLocation();
+    const current_route = location.pathname
     
     const remove_cart = (e) => {
         const id = e.target.id
@@ -74,67 +78,70 @@ const TopLogoComponent = () => {
                         <div className="col-md-6 free-shipping-col" style={{position: "relative"}}>
                             <div className="free-shipping">
                                 <div className="row">
-
-                                    <div className="col-md-12" style={{position: "relative"}}>
-                                        <div className="cart_block">
-                                            <ul>
-                                                { JSON.parse(localStorage.getItem("cart")) &&
-                                                <li className="cart_box" id="cart_box" >
-                                                    {
-                                                        JSON.parse(localStorage.getItem("cart")).length != 0 &&
-                                                        <div className="cart_count" id="cart_count">
-                                                            <p>
-                                                            {
-                                                                JSON.parse(localStorage.getItem("cart")).length
-                                                            }
-                                                            </p>
-                                                        </div>
-                                                    }
-                                                    
-                                                    <i className="fas fa-shopping-basket"></i>
-
-                                                    {
-                                                        JSON.parse(localStorage.getItem("cart")).length != 0 &&
-                                                        <div className="cart_list" id="cart_list">
-                                                            <table className="table table-striped">
-                                                                
-                                                                <tbody id="cart_list_body">
-                                                                    {
-                                                                        JSON.parse(localStorage.getItem("cart")).map( (value, index)  => (
-                                                                            <tr>
-                                                                                <td>
-                                                                                    <img src={value.image} width="50px" alt=""></img>
-                                                                                </td>
-                                                                                <td>
-                                                                                    <a href="#">{value.name}</a>
-                                                                                    <p>{value.qty}x {value.price}</p>
-                                                                                </td>
-                                                                                <td>
-                                                                                    <button className="remove_cart" onClick={remove_cart} >
-                                                                                        <i className="fas fa-times" id={value.id}></i>
-                                                                                    </button>
-                                                                                </td>
-                                                                            </tr>
-                                                                        ))
-                                                                    }
-                                                                </tbody>
-                                                            </table>
-
-                                                            <div className="cart_detail">
-                                                                <Link to="/checkout" className="checkout">Checkout</Link>
+                                    {
+                                        current_route != '/checkout' && 
+                                        <div className="col-md-12" style={{position: "relative"}}>
+                                            <div className="cart_block">
+                                                <ul>
+                                                    { JSON.parse(localStorage.getItem("cart")) &&
+                                                    <li className="cart_box" id="cart_box" >
+                                                        {
+                                                            JSON.parse(localStorage.getItem("cart")).length != 0 &&
+                                                            <div className="cart_count" id="cart_count">
+                                                                <p>
+                                                                {
+                                                                    JSON.parse(localStorage.getItem("cart")).length
+                                                                }
+                                                                </p>
                                                             </div>
+                                                        }
+                                                        
+                                                        <i className="fas fa-shopping-basket"></i>
 
-                                                        </div>
+                                                        {
+                                                            JSON.parse(localStorage.getItem("cart")).length != 0 &&
+                                                            <div className="cart_list" id="cart_list">
+                                                                <table className="table table-striped">
+                                                                    
+                                                                    <tbody id="cart_list_body">
+                                                                        {
+                                                                            JSON.parse(localStorage.getItem("cart")).map( (value, index)  => (
+                                                                                <tr>
+                                                                                    <td>
+                                                                                        <img src={value.image} width="50px" alt=""></img>
+                                                                                    </td>
+                                                                                    <td>
+                                                                                        <a href="#">{value.name}</a>
+                                                                                        <p>{value.qty}x {value.price}</p>
+                                                                                    </td>
+                                                                                    <td>
+                                                                                        <button className="remove_cart" onClick={remove_cart} >
+                                                                                            <i className="fas fa-times" id={value.id}></i>
+                                                                                        </button>
+                                                                                    </td>
+                                                                                </tr>
+                                                                            ))
+                                                                        }
+                                                                    </tbody>
+                                                                </table>
+
+                                                                <div className="cart_detail">
+                                                                    <Link to="/checkout" className="checkout">Checkout</Link>
+                                                                </div>
+
+                                                            </div>
+                                                        }
+                                                        
+                                                        
+
+                                                    </li>
                                                     }
-                                                    
-                                                    
+                                                </ul>
 
-                                                </li>
-                                                }
-                                            </ul>
-
+                                            </div>
                                         </div>
-                                    </div>
+                                    }
+                                    
 
                                 </div>
                             </div>
